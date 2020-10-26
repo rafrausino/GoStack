c443c9e1f43da1f39039c835eb13c7d1570cf8f5
Renan Afrausino <renan.afrausino@gmail.com>
2020-10-26 17:51:18 -0300

    feat(node): creating project backend with node.js

---
+++ nivel-01/01-backend-com-node/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello GoStack'});
+})
+app.listen(3333, () => {
+  console.log('🚀 Back-end started')
+});
+
