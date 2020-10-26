ed25a13b34ebd16f043382c4c8bbc82b5559f02c
Renan Afrausino <renan.afrausino@gmail.com>
2020-10-26 18:38:09 -0300

    feat(node): Using the Insomnia

--- nivel-01/01-backend-com-node/src/index.js
+++ nivel-01/01-backend-com-node/src/index.js
@@ -2,9 +2,33 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello GoStack'});
+/**
+ * Métodos: HTTPs
+ * 
+ * GET: Buscar informações do back-end
+ * POST: Criar uma informação no back-end
+ * PUT/PATCH: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ * 
+ */
+
+app.get('/projects', (request, response) => {
+  return response.json(["Projeto 1", "Projeto 2"]);
 })
+
+app.post('/projects', (request, response) => {
+  return response.json(["Projeto 1", "Projeto 2", 'projeto 3'])
+})
+
+app.put("/projects/:id", (request, response) => {
+  return response.json(["Projeto 4", "Projeto 5", "projeto 6"]);
+});
+
+app.delete("/projects/:id", (request, response) => {
+  return response.json(["Projeto 5", "Projeto 2"]);
+});
+
+
 app.listen(3333, () => {
   console.log('🚀 Back-end started')
 });
