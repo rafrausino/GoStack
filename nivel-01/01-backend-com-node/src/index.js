f616af8a4d768e46d3aa2f506b9873814d10bbb4
Renan Afrausino <renan.afrausino@gmail.com>
2020-10-26 19:00:30 -0300

    feat(node): Parameter types in the Insomnia

--- nivel-01/01-backend-com-node/src/index.js
+++ nivel-01/01-backend-com-node/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 /**
  * Métodos: HTTPs
  * 
@@ -12,15 +14,37 @@ const app = express();
  * 
  */
 
+/**
+  * Tipos de parâmetros
+  * 
+  * Query Params: Filtros e paginação
+  * Route Params: Identificaqr recurso (Atualizar/Deletar)
+  * Request Body: Conteúdo na hora criar ou editar um recurso (JSON)
+  */
+
 app.get('/projects', (request, response) => {
+  const { title, owner } = request.query;
+
+  console.log(title);
+  console.log(owner);
+
   return response.json(["Projeto 1", "Projeto 2"]);
 })
 
 app.post('/projects', (request, response) => {
+  const { title, owner } = request.body; 
+
+  console.log(title);
+  console.log(owner);
   return response.json(["Projeto 1", "Projeto 2", 'projeto 3'])
 })
 
 app.put("/projects/:id", (request, response) => {
+
+  const { id } = request.params;
+
+  console.log(id)
+
   return response.json(["Projeto 4", "Projeto 5", "projeto 6"]);
 });
 
