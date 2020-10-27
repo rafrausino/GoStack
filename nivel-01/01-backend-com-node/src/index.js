cc46096ccbdf55020f22b9cc9dbf315527e648a1
Renan Afrausino <renan.afrausino@gmail.com>
2020-10-27 14:26:32 -0300

    feat(node): application functional

--- nivel-01/01-backend-com-node/src/index.js
+++ nivel-01/01-backend-com-node/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -22,34 +23,64 @@ app.use(express.json());
   * Request Body: Conteúdo na hora criar ou editar um recurso (JSON)
   */
 
+  const projects = [];
+
 app.get('/projects', (request, response) => {
-  const { title, owner } = request.query;
+  const { title } = request.query;
 
-  console.log(title);
-  console.log(owner);
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
 
-  return response.json(["Projeto 1", "Projeto 2"]);
+  return response.json(results);
 })
 
 app.post('/projects', (request, response) => {
   const { title, owner } = request.body; 
 
-  console.log(title);
-  console.log(owner);
-  return response.json(["Projeto 1", "Projeto 2", 'projeto 3'])
+  const project = { id: uuid(), title, owner} ; 
+
+  projects.push(project);
+   
+  return response.json(project);
 })
 
 app.put("/projects/:id", (request, response) => {
 
   const { id } = request.params;
+  const { title, owner } = request.body;
 
-  console.log(id)
+  const projectIndex = projects.findIndex(project => project.id === id)
 
-  return response.json(["Projeto 4", "Projeto 5", "projeto 6"]);
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.' });
+  }
+
+  const project = { 
+    id,
+    title, 
+    owner 
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
 app.delete("/projects/:id", (request, response) => {
-  return response.json(["Projeto 5", "Projeto 2"]);
+
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found." });
+  }
+
+  projects.splice(projectIndex, 1)
+
+  return response.status(204).send();
+
 });
 
 
