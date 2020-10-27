7937270cd5724937f5548afacfd9f0b9de301155
Renan Afrausino <renan.afrausino@gmail.com>
2020-10-27 18:39:35 -0300

    feat(node): Middlewares

--- nivel-01/01-backend-com-node/src/index.js
+++ nivel-01/01-backend-com-node/src/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const { uuid } = require('uuidv4');
+const express = require("express");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -7,59 +7,89 @@ app.use(express.json());
 
 /**
  * Métodos: HTTPs
- * 
+ *
  * GET: Buscar informações do back-end
  * POST: Criar uma informação no back-end
  * PUT/PATCH: Alterar uma informação no back-end
  * DELETE: Deletar uma informação no back-end
- * 
+ *
  */
 
 /**
-  * Tipos de parâmetros
-  * 
-  * Query Params: Filtros e paginação
-  * Route Params: Identificaqr recurso (Atualizar/Deletar)
-  * Request Body: Conteúdo na hora criar ou editar um recurso (JSON)
-  */
+ * Tipos de parâmetros
+ *
+ * Query Params: Filtros e paginação
+ * Route Params: Identificaqr recurso (Atualizar/Deletar)
+ * Request Body: Conteúdo na hora criar ou editar um recurso (JSON)
+ */
 
-  const projects = [];
+/**
+ * Middleware:
+ *
+ * Interceptador de requisições que interromper totalmente a requisição ou alterar dados da requisição.
+ */
 
-app.get('/projects', (request, response) => {
+const projects = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel);
+
+  next(); // Próximo middleware
+
+  console.timeEnd(logLabel);
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID.' });
+  }
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId)
+
+app.get("/projects", (request, response) => {
+
   const { title } = request.query;
 
   const results = title
-    ? projects.filter(project => project.title.includes(title))
+    ? projects.filter((project) => project.title.includes(title))
     : projects;
 
   return response.json(results);
-})
+});
 
-app.post('/projects', (request, response) => {
-  const { title, owner } = request.body; 
+app.post("/projects", (request, response) => {
+  const { title, owner } = request.body;
 
-  const project = { id: uuid(), title, owner} ; 
+  const project = { id: uuid(), title, owner };
 
   projects.push(project);
-   
+
   return response.json(project);
-})
+});
 
 app.put("/projects/:id", (request, response) => {
-
   const { id } = request.params;
   const { title, owner } = request.body;
 
-  const projectIndex = projects.findIndex(project => project.id === id)
+  const projectIndex = projects.findIndex((project) => project.id === id);
 
   if (projectIndex < 0) {
-    return response.status(400).json({ error: 'Project not found.' });
+    return response.status(400).json({ error: "Project not found." });
   }
 
-  const project = { 
+  const project = {
     id,
-    title, 
-    owner 
+    title,
+    owner,
   };
 
   projects[projectIndex] = project;
@@ -68,7 +98,6 @@ app.put("/projects/:id", (request, response) => {
 });
 
 app.delete("/projects/:id", (request, response) => {
-
   const { id } = request.params;
 
   const projectIndex = projects.findIndex((project) => project.id === id);
@@ -77,14 +106,11 @@ app.delete("/projects/:id", (request, response) => {
     return response.status(400).json({ error: "Project not found." });
   }
 
-  projects.splice(projectIndex, 1)
+  projects.splice(projectIndex, 1);
 
   return response.status(204).send();
-
 });
-
 
 app.listen(3333, () => {
-  console.log('🚀 Back-end started')
+  console.log("🚀 Back-end started");
 });
-
